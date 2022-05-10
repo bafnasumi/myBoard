153c3e73b2efb61faf9761e5dac37f4c72b408d7
Rishabh <44425012+ioxvar@users.noreply.github.com>
2022-05-11 02:42:03 +0530

    sign out button

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myboardapp/pages/loggedin.dart';
@@ -14,10 +16,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        // backgroundColor: Color.fromARGB(255, 199, 149, 0),
-        body: SafeArea(
-      child: Text('Lets make something beautiful'),
-    ));
+    return Scaffold(
+      // backgroundColor: Color.fromARGB(255, 199, 149, 0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Lets make something beautiful!!'),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.teal.shade400,
+              child: Text('Sign Out'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
