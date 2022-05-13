bc729687c6ea779406ca77598e1a01b1954881ae
Rishabh <44425012+ioxvar@users.noreply.github.com>
2022-05-14 01:25:00 +0530

    links init

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,7 +2,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import './newpages.dart';
+import 'pagenavigator.dart';
 // import 'package:myboardapp/pages/loggedin.dart';
 // import 'package:myboardapp/pages/loginpage.dart';
 // import 'package:myboardapp/services/google_sign_in.dart';
@@ -19,25 +19,181 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Color.fromARGB(255, 199, 149, 0),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Lets make something beautiful!!'),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.teal.shade400,  
-              child: Text('Sign Out'),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 10, 75, 107),
+        title: Text(
+          'Home',
+          style: TextStyle(),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ), //BoxDecoration
+                          child: Center(
+                            child: GestureDetector(
+                              onTap:() => Navigator.pushNamed(context, '/links'),
+                              child: Text(
+                                'Links',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ), //Container
+                      ), //Flexible
+                      SizedBox(
+                        width: 20,
+                      ), //SizedBox
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.loose,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Board 2',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ), //BoxDecoration
+                        ), //Container
+                      ) //Flexible
+                    ], //<Widget>[]
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ), //Row
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ), //BoxDecoration
+                          child: Center(
+                            child: Text(
+                              'Board 3',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ), //Container
+                      ), //Flexible
+                      SizedBox(
+                        width: 20,
+                      ), //SizedBox
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.loose,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Board 4',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ), //BoxDecoration
+                        ), //Container
+                      ) //Flexible
+                    ], //<Widget>[]
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ), //Row
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ), //BoxDecoration
+                          child: Center(
+                            child: Text(
+                              'Board 5',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ), //Container
+                      ), //Flexible
+                      SizedBox(
+                        width: 20,
+                      ), //SizedBox
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.loose,
+                        child: Container(
+                          height: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blueGrey,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Board 6',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ), //BoxDecoration
+                        ), //Container
+                      ) //Flexible
+                    ], //<Widget>[]
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ), //Row
+                Text('Lets make something beautiful!!'),
+                MaterialButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  color: Colors.teal.shade400,
+                  child: Text('Sign Out'),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).push(NewPage(1));
+                  },
+                  child: Text("todo"),
+                )
+              ],
             ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.of(context).push(NewPage(1));
-              },
-              child: Text("todo"),
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/pages/links.dart
+++ lib/pages/links.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Links extends StatefulWidget {
   const Links({Key? key}) : super(key: key);
@@ -8,14 +9,90 @@ class Links extends StatefulWidget {
 }
 
 class _LinksState extends State<Links> {
+  final _linkController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Links',
+          style: TextStyle(fontSize: 24),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       backgroundColor: Colors.amber,
-      body: Container(
-          height: MediaQuery.of(context).size.height * 0.4,
-          width: 30.0,
-          child: const Text('homepage')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    // SizedBox(height: 50),
+                    
+                   const SizedBox(height: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Links are important! ',
+                          style: GoogleFonts.lato(
+                            fontSize: 26,
+                          ),
+                        ),
+                        Text(
+                          '(YouTube, Instagram, Pinterest or others)',
+                          style: GoogleFonts.lato(
+                            
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          controller: _linkController,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Link'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          maxLines: 4,
+                          controller: _linkController,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Description'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
